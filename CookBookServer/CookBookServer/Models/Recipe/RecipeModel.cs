f6aec5776741e515021b06f45c86b75a9e9eb6b7
Nikita <polkovnikovnikita99@yandex.ru>
2020-11-14 16:34:11 +0300

    RecipesRepository

--- CookBookServer/CookBookServer/Models/Recipe/RecipeModel.cs
+++ CookBookServer/CookBookServer/Models/Recipe/RecipeModel.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using CookBookServer.Interfaces;
+using System;
 
 namespace CookBookServer.Models.Recipe
 {
-    public class RecipeModel
+    public class RecipeModel : IHasStringId
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; }
+        public string ImageName { get; set; }
         public string Description { get; set; }
-        public string Category { get; set; }
-        public List<string> TagsList { get; set; }
+        //public string Category { get; set; }
+        //public IEnumerable<string> TagsList { get; set; }
         public DateTime PublicationDate { get; set; }
         public DateTime CookingTime { get; set; }
-        public List<CheckpointModel> Checkpoints { get; set; }
-        public List<IngredientModel> Ingredients { get; set; }
+        public IEnumerable<CheckpointModel> Checkpoints { get; set; }
+        public IEnumerable<IngredientModel> Ingredients { get; set; }
         public Dictionary<int, int> Raitings { get; set; }
     }
 }
