b16bc14a24e544daa405374d1ef0b83108c6310c
Anastasia Nikitina <nickitinanastya@gmail.com>
2020-11-14 15:38:26 +0300

    +

--- CookBookServer/CookBookServer/Controllers/RecipeController.cs
+++ CookBookServer/CookBookServer/Controllers/RecipeController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CookBookServer.Controllers
 {
+    [Authorize]
     public class RecipeController : Controller
     {
         // GET: RecipeController
+      
         public IActionResult Index()
-        {            
+        {
+            var keke = User.Identity.IsAuthenticated;
             return View();
         }
     }
