0eaf23470a5b595017fb9349631ef56b5f530901
Nikita <polkovnikovnikita99@yandex.ru>
2020-11-14 15:35:29 +0300

    разделение бэка: старт разработки

---
+++ CookBookServer/CookBackApi/Controllers/RecipeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookBackApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecipeController : ControllerBase
+    {
+
+        private readonly ILogger<RecipeController> _logger;
+
+        public RecipeController(ILogger<RecipeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
