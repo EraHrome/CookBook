9f3f690a52f23217234c8c5643ba1b31b6fcbc88
Anastasia Nikitina <nickitinanastya@gmail.com>
2020-11-15 05:43:48 +0300

    no message

--- CookBookServer/Mongo/Models/Recipe/CheckpointModel.cs
+++ CookBookServer/Mongo/Models/Recipe/CheckpointModel.cs
@@ -5,9 +5,19 @@ namespace Mongo.Models.Recipe
 {
     public class CheckpointModel
     {
+        public CheckpointModel()
+        {
+            Ingredients = new List<IngredientModel>();
+        }
+
         public string CookingSeconds { get; set; }
         public string Description { get; set; }
         public string TimerSeconds { get; set; }
         public IEnumerable<IngredientModel> Ingredients { get; set; }
+
+        [NotMapped]
+        public int TimerMinutes { get; set; }
+        [NotMapped]
+        public int CookingMinutes { get; set; }
     }
 }
